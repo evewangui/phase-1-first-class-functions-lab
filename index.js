dbc76c9fe8af54b8b3cbc7ccf56051a72fbb1dca
eve <evewangui447@gmail.com>
2024-10-01 22:51:17 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+// Function to return the first two drivers
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0,2);
+}
+
+// Function to return the last two drivers
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+}
+
+// Array containing the two driver functions
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+//Function that returns a fare multiplier
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    };
+}
+
+// Function to double the fare
+const fareDoubler = createFareMultiplier(2);
+
+// Function to triple the fare
+const fareTripler = createFareMultiplier(3);
+
+// Function to select different drivers based on a function
+function selectDifferentDrivers(arrayOfDrivers, fn) {
+    return fn(arrayOfDrivers);
+}
